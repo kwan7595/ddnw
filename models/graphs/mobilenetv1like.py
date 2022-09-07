9.7 fix - add threshold ->removed p from prune_val - masking function at util.py - loss scaling ->lamda value with 1e-3

models/graphs/mobilenetv1like.py
@@ -13,7 +13,7 @@ if getattr(FLAGS, 'use_dgl', False):
 
 
 class Block(nn.Module):
-    def __init__(self, inp, oup, stride, blocks):
+    def __init__(self, inp, oup, stride, blocks,threshold):
         super(Block, self).__init__()
 
         self.inp = inp
@@ -23,6 +23,9 @@ class Block(nn.Module):
 
         self.stride = stride
         self.blocks = blocks
+
+        ##threshold for edge selection
+        self.threshold = threshold
 
         self.fast_eval = False
 
@@ -68,6 +71,7 @@ class Block(nn.Module):
             self.oup * self.blocks,
             self.inp,
             self.oup,
+            self.threshold
         )
 
         self.prune_rate = prune_rate
@@ -174,6 +178,10 @@ class Block(nn.Module):
 
     def get_weight(self):
         return self.graph.get_weight()
+
+    ## weight loss for resource
+    def get_weight_loss(self):
+        return self.graph.get_weight_loss()
 
     def forward(self, x):
         if self.fast_eval:
@@ -324,14 +332,18 @@ class Block(nn.Module):
 
 
 class Linear(nn.Module):
-    def __init__(self, inp):
+    def __init__(self, inp,threshold):
         super(Linear, self).__init__()
         self.inp = inp
         self.oup = FLAGS.output_size
         self.graph = get_graph(FLAGS.prune_rate, inp, FLAGS.output_size)
+        self.threshold = threshold
 
     def get_weight(self):
         return self.graph.get_weight()
+
+    def get_weight_loss(self):
+        return self.graph.get_weight_loss()
 
     def profiling(self):
         w = self.get_weight().squeeze().t()
@@ -356,7 +368,7 @@ class MobileNetV1Like(nn.Module):
         (512, 1024, 2, 2),
     ]
 
-    def __init__(self,):
+    def __init__(self,threshold):
         super(MobileNetV1Like, self).__init__()
         self.conv1 = nn.Conv2d(
             3,
@@ -375,12 +387,12 @@ class MobileNetV1Like(nn.Module):
             self.linear = nn.Linear(1024, FLAGS.output_size)
         self.relu = nn.ReLU(inplace=True)
         self.bn = nn.BatchNorm2d(1024)
-
+        self.threshold = 0.006
     def _make_layers(self):
         blocks = []
         for x in self.cfg:
             inp, oup, stride, layers = x
-            blocks.append(Block(inp, oup, stride, layers))
+            blocks.append(Block(inp, oup, stride, layers,self.threshold))
 
         return nn.Sequential(*blocks)
 
@@ -391,6 +403,14 @@ class MobileNetV1Like(nn.Module):
         if hasattr(self.linear, "get_weight"):
             out.append(self.linear.get_weight())
         return out
+
+    ## get weight loss for resource constraint
+    def get_weight_loss(self):
+        out = 0
+        for layer in self.layers:
+            out+=layer.get_weight_loss()
+        if hasattr(self.linear, "get_weight"):
+            out+=self.linear.get_weight_loss()
 
     def forward(self, x):
         out = self.conv1(x)

models/graphs/util.py
@@ -18,10 +18,10 @@ def get_conv(inp, oup):
 
 
 class Graph(nn.Conv2d):
-    def __init__(self, prune_rate, dim_in, dim_out):
+    def __init__(self, prune_rate, dim_in, dim_out,threshold):
         super(Graph, self).__init__(dim_in, dim_out, kernel_size=1, bias=False)
         self.prune_rate = prune_rate
-
+        self.threshold = threshold
     def get_weight(self):
         return self.weight
 
@@ -34,7 +34,7 @@ class Graph(nn.Conv2d):
 
     ###code for resource-constraint loss : L1 loss on edge weight, activation with tanh
     def get_weight_loss(self):
-        out = F.tanh(self.weight).abs().sum()  ## do i need to detach?
+        out = F.tanh(self.weight).abs().sum()  ## weight is randomly initialized -> loss explodes with tanh. ->scaled..
         return out
 
 ########################################################################################################################
@@ -77,14 +77,16 @@ class RandomGraph(Graph):
 
 class ChooseTopEdges(autograd.Function):
     """ Chooses the top edges for the forwards pass but allows gradient flow to all edges in the backwards pass"""
-
+    ## add tau - threshold value to choose edges those weights are higher than t
     @staticmethod
-    def forward(ctx, weight, prune_rate):
+    def forward(ctx, weight, prune_rate,threshold):
         output = weight.clone()
         _, idx = weight.flatten().abs().sort()
-        p = int(prune_rate * weight.numel())
+        #p = int(prune_rate * weight.numel())
         flat_oup = output.flatten()
-        flat_oup[idx[:p]] = 0
+        #flat_oup[idx[:p]] = 0
+        mask = nn.Threshold(threshold,0)
+        flat_oup = mask(flat_oup)
         return output
 
     @staticmethod
@@ -93,11 +95,11 @@ class ChooseTopEdges(autograd.Function):
 
 
 class DNW(Graph):
-    def __init__(self, prune_rate, dim_in, dim_out):
-        super().__init__(prune_rate, dim_in, dim_out)
+    def __init__(self, prune_rate, dim_in, dim_out,threshold):
+        super().__init__(prune_rate, dim_in, dim_out,threshold)
 
     def get_weight(self):
-        return ChooseTopEdges.apply(self.weight, self.prune_rate)
+        return ChooseTopEdges.apply(self.weight, self.prune_rate,self.threshold)
 
 
 ########################################################################################################################
@@ -256,11 +258,11 @@ class Complete(Graph):
 ########################################################################################################################
 
 
-def get_graph(prune_rate, dim_in, dim_out, in_channels=None, out_channels=None):
+def get_graph(prune_rate, dim_in, dim_out, in_channels=None, out_channels=None,threshold=0.006):
     if FLAGS.graph == "random":
         return RandomGraph(prune_rate, dim_in, dim_out, in_channels, out_channels)
     elif FLAGS.graph == "dnw":
-        return DNW(prune_rate, dim_in, dim_out)
+        return DNW(prune_rate, dim_in, dim_out,threshold)
     elif FLAGS.graph == "dnw_no_update":
         return DNWNoUpdate(prune_rate, dim_in, dim_out, in_channels, out_channels)
     elif FLAGS.graph == "reg_td":

train.py
@@ -233,7 +233,6 @@ def forward_loss(model, criterion, input, target, meter):
         correct_k = correct[:k].float().sum(0)
         accuracy_list = list(correct_k.cpu().detach().numpy())
         meter["top{}_accuracy".format(k)].cache_list(accuracy_list)
-
     return loss
 
 
@@ -293,16 +292,15 @@ def run_one_epoch(
             iter += 1
 
             optimizer.zero_grad()
-
-            loss = forward_loss(model, criterion, input, target, meters) + model.get_weight_loss()
-            ### tanh loss for resource constraint.
+            ## resource constraint loss.
+            loss = forward_loss(model, criterion, input, target, meters) + (1e-3)*model.module.get_weight_loss()
             loss.backward()
             optimizer.step()
 
         else:
             ############################### VAL #################################
 
-            loss = forward_loss(model, criterion, input, target, meters) + model.get_weight_loss()
+            loss = forward_loss(model, criterion, input, target, meters) + (1e-3)*model.module.get_weight_loss()
 
         batch_time = time.time() - end
         end = time.time()
@@ -381,6 +379,7 @@ def train_val_test():
     criterion = torch.nn.CrossEntropyLoss(reduction="none").to(device)
 
     print("=> creating model '{}'".format(FLAGS.model))
+    ##add threshold as param.
     model = getter("model")()
 
     optimizer = get_optimizer(model)