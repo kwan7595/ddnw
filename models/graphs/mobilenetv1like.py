fixed edge pruning code - masking fix - no clone - profiling fix - count edge those weight valuse over threshold(theoretical flops) - fixed train.py ->small-scale graphs are not iterable, thus called model.module.profiling directly

models/graphs/mobilenetv1like.py
@@ -83,7 +83,8 @@ class Block(nn.Module):
         # graph ops.
         w = self.get_weight().squeeze().t()
         #num_edges = w.size(0) * w.size(1) * (1 - self.graph.prune_rate)
-        num_edges = w.size(0)*w.size(1)(1-w.count(0)) #remove edge
+        mask = w.ge(FLAGS.threshold)  # ccount edges those weights are over threshold
+        num_edges = torch.masked_select(w, mask).numel()
         graph_n_macs = num_edges * spatail * spatail
         graph_n_params = num_edges
 
@@ -348,7 +349,10 @@ class Linear(nn.Module):
 
     def profiling(self):
         w = self.get_weight().squeeze().t()
-        num_edges = int(w.size(0) * w.size(1) * (1 - self.graph.prune_rate))
+        #num_edges = int(w.size(0) * w.size(1) * (1 - self.graph.prune_rate))
+        #num_edges = w.size(0)*w.size(1)*(len(w)-len(torch.nonzero(w))) #remove edge
+        mask = w.ge(FLAGS.threshold)  # ccount edges those weights are over threshold
+        num_edges = torch.masked_select(w, mask).numel()
         n_macs = num_edges
         n_params = num_edges
         return n_macs, n_params, None

models/graphs/neuralgraph.py
@@ -86,7 +86,9 @@ class DiscreteTimeNeuralGraph(nn.Module):
 
     def profiling(self):
         w = self.get_weight().squeeze().t()
-        num_edges = w.size(0) * w.size(1) * (1 - self.graph.prune_rate)
+        #num_edges = w.size(0) * w.size(1) * (1 - self.graph.prune_rate)
+        mask = w.ge(FLAGS.threshold)
+        num_edges = torch.masked_select(w,mask).numel()
         graph_n_params = num_edges
 
         has_output = (w.abs().sum(1) != 0)
@@ -281,6 +283,23 @@ class StaticNeuralGraph(nn.Module):
     ### get edge weight loss
     def get_weight_loss(self):
         return self.graph.get_weight_loss()
+    ## profiling for small-scale static-neural graph with dnw
+    def profiling(self):
+        w = self.get_weight().squeeze().t()
+        #num_edges = w.size(0) * w.size(1) * (1 - self.graph.prune_rate)
+        mask = w.ge(FLAGS.threshold) # ccount edges those weights are over threshold
+        num_edges = torch.masked_select(w,mask).numel()
+        graph_n_params = num_edges
+
+        has_output = (w.abs().sum(1) != 0)
+        has_input = (w.abs().sum(0) != 0)
+
+        # node ops. no ops at output nodes.
+        num_nodes_with_ops = has_output.sum()
+        node_n_params = num_nodes_with_ops * 3 * 3
+        n_params = int(node_n_params + graph_n_params)
+
+        return n_params
 
     def forward(self, x):
         x = torch.cat(

models/graphs/util.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import autograd
-
+import torch.nn.utils.prune as prune
 from genutil.config import FLAGS
 
 
@@ -36,7 +36,6 @@ class Graph(nn.Conv2d):
     def get_weight_loss(self):
         out = F.tanh(self.weight).abs().sum()  ## weight is randomly initialized -> loss explodes with tanh. ->scaled..
         return out
-
 ########################################################################################################################
 # Random Graph                                                                                                         #
 ########################################################################################################################
@@ -80,13 +79,13 @@ class ChooseTopEdges(autograd.Function):
     ## add tau - threshold value to choose edges those weights are higher than t
     @staticmethod
     def forward(ctx, weight, prune_rate,threshold):
-        output = weight.clone() # for masking
+        output = weight
         #_, idx = weight.flatten().abs().sort()
         #p = int(prune_rate * weight.numel())
         flat_oup = output.flatten()
         #flat_oup[idx[:p]] = 0
         mask = nn.Threshold(threshold,0,inplace=True)
-        mask(output) # inplace
+        mask(flat_oup) # inplace
         return output
 
     @staticmethod