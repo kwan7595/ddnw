final fix - currently on large-scale with t=0.3 - pruning forward ->used clone() to return hallucinated edges - weight is updated at backprop - at validation, profiling fixed   - get_weight() applies chooseTopEdge -> return hallucinated edges   - count edges with nonzero values - small-scale doesn't work well (param decreases,but accuracy is same) - add param at profiling step

models/graphs/neuralgraph.py
@@ -287,8 +287,7 @@ class StaticNeuralGraph(nn.Module):
     def profiling(self):
         w = self.get_weight().squeeze().t()
         #num_edges = w.size(0) * w.size(1) * (1 - self.graph.prune_rate)
-        mask = w.ge(FLAGS.threshold) # ccount edges those weights are over threshold
-        num_edges = torch.masked_select(w,mask).numel()
+        num_edges = w.count_nonzero().item()
         graph_n_params = num_edges
 
         has_output = (w.abs().sum(1) != 0)

models/graphs/util.py
@@ -79,13 +79,14 @@ class ChooseTopEdges(autograd.Function):
     ## add tau - threshold value to choose edges those weights are higher than t
     @staticmethod
     def forward(ctx, weight, prune_rate,threshold):
-        output = weight
+        output = weight.clone()
         #_, idx = weight.flatten().abs().sort()
         #p = int(prune_rate * weight.numel())
-        flat_oup = output.flatten()
+        abs_out = output.abs() #flatten with absolute values
+        mask = abs_out.le(threshold) # create mask, s.t value>threshold ->false, else true
+        #l1_threshold= torch.norm(output,p=1)/weight.numel()*threshold# ->consider 1l norm as a threshold
         #flat_oup[idx[:p]] = 0
-        mask = nn.Threshold(threshold,0,inplace=True)
-        mask(flat_oup) # inplace
+        output.masked_fill_(mask,0) # replace weight with 0 if True(less or equal to threshold)
         return output
 
     @staticmethod