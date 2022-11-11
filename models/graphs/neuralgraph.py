11.11(Fri) - profiling fix ->considering weight matrix - sigmoid scaling ->considering weight TODO - probability based flops profiling fix

models/graphs/neuralgraph.py
@@ -283,32 +283,28 @@ class StaticNeuralGraph(nn.Module):
     def get_original_weight(self):
         return self.graph.get_original_weight()
     ### get edge weight loss
-    def get_weight_loss(self):
-        return self.graph.get_weight_loss()
-    ## profiling for small-scale static-neural graph with dnw
-    #def expected_profiling(self): ## need to change names
-        # weight = self.graph.get_original_weight().squeeze().t()
-        # sigmoid = nn.Sigmoid()
-        # edge_prob = sigmoid(weight.abs() - FLAGS.threshold)
-        # graph_n_params = torch.sum(edge_prob)
-        # out_node_prob = torch.prod(1 - edge_prob, 0)
-        # expected_node_n_params = torch.sum(((1 - out_node_prob) * 3 * 3))  # expected param
-        # n_params = expected_node_n_params + graph_n_params
-        #return n_params
+    def get_block_rng(self):
+        return self.block_rng
 
-    def profiling(self): # real-profiling(no approximation)
-        w = self.graph.get_weight().squeeze().t()
-        num_edges = w.count_nonzero().item()
-        graph_n_params = num_edges
-
-        has_output = (w.abs().sum(1)!=0)
-        has_input = (w.abs().sum(0)!=0)
-
-        num_nodes_with_ops = has_output.sum()
+    def profiling(self): # real-profiling(no approximation), considering small-scale edge weight matrix
+        w = self.graph.get_weight().squeeze()
+        num_edges = 0
+        graph_n_params = 0
+        num_nodes_with_ops = 0
+        for i in range(self.layers):
+            layer_weight=w[
+            min(self.block_rng[i + 1], self.dim - self.feature_dim):,
+            self.block_rng[i]: self.block_rng[i + 1],
+            ]
+            num_edge = layer_weight.count_nonzero().item()
+            num_edges+=num_edge
+            graph_n_params+=num_edge
+            has_output = layer_weight.abs().sum(1)!=0
+            has_input = layer_weight.abs().sum(0)!=0
+            num_nodes_with_ops +=has_output.sum()
         node_n_params = num_nodes_with_ops * 3 * 3
 
         n_params = int(node_n_params + graph_n_params)
-
         return n_params
 
     def forward(self, x):

models/graphs/util.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from torch import autograd
 import torch.functional as F
-import torch.nn.utils.prune as prune
 from genutil.config import FLAGS
 
 
@@ -13,20 +12,28 @@ def get_conv(inp, oup):
 
 
 class Flops_Loss(nn.Module): #class for flops-based loss implementation,
-    def __init__(self,threshold,max_params):
+    def __init__(self,threshold,max_params,feature_dim):
         super(Flops_Loss,self).__init__()
         self.threshold = threshold
         self.max_params = max_params
-    def forward(self,weight):
-        sigmoid = nn.Sigmoid()
-        edge_prob = sigmoid(weight.abs() - self.threshold)
-        graph_n_params = edge_prob.sum()
-        out_node_prob = torch.prod(1 - edge_prob, 0)
-        expected_node_n_params = ((1 - out_node_prob) * 3 * 3).sum()  # expected param
+        self.feature_dim = feature_dim
+    def forward(self,weight,block_rng):
+        graph_n_params = 0
+        expected_node_n_params = 0
+        for i in range(FLAGS.layers):
+            layer_weight = weight[
+                           min(block_rng[i + 1], FLAGS.dim - self.feature_dim):,
+                           block_rng[i]: block_rng[i + 1],
+                           ]
+            sigmoid = nn.Sigmoid()
+            edge_prob = sigmoid(FLAGS.alpha*(layer_weight.abs() - self.threshold))
+            graph_n_params += edge_prob.sum()
+            out_node_prob = torch.prod(1 - edge_prob, 1)
+            expected_node_n_params += ((1 - out_node_prob) * 3 * 3).sum()  # expected param
         n_params = expected_node_n_params + graph_n_params
+        print(n_params.item())
         g = nn.Softplus()
         loss_flops = g(n_params-self.max_params)
-        #print(edge_prob.shape,edge_prob.mean(),out_node_prob.shape,n_params)
         return loss_flops
 ########################################################################################################################
 # Graph Superclass                                                                                                     #
@@ -125,8 +132,6 @@ class DNW(Graph):
     def get_original_weight(self):
         return self.weight
 
-    def get_flops_loss(self):
-        return Flops_loss(self.weight)
 ########################################################################################################################
 # DNW without an update rule on the backwards pass                                                                     #
 ########################################################################################################################

train.py
@@ -21,7 +21,6 @@ from genutil.config import FLAGS
 from genutil.model_profiling import model_profiling,model_expected_profiling
 from models.graphs.util import Flops_Loss
 from tensorboardX import SummaryWriter
-
 best_acc1 = 0
 writer = None
 
@@ -236,10 +235,12 @@ def forward_loss(model, criterion, input, target, meter):
         meter["top{}_accuracy".format(k)].cache_list(accuracy_list)
     return loss
 
-def get_flops_loss(weight,flops_criterion,meter):
-    fl = flops_criterion(weight) * FLAGS.beta
-    meter["FlopsLoss"].cache(fl.cpu().detach().numpy())
-    return fl
+def get_flops_loss(weight,flops_criterion,meter,block_rng): #layer-wise weight calculation?
+    ##block_range, feature_dim, for-loop
+    loss= flops_criterion(weight,block_rng)
+    loss = loss*FLAGS.beta
+    meter["FlopsLoss"].cache(loss.cpu().detach().numpy())
+    return loss
 
 def run_one_epoch(
     epoch,
@@ -294,8 +295,8 @@ def run_one_epoch(
             iter += 1
 
             optimizer.zero_grad()
-            edge_weight = model_profiling(model.module)
-            flops_loss = get_flops_loss(edge_weight,flops_criterion,meters)
+            edge_weight,block_rng = model_profiling(model.module)
+            flops_loss = get_flops_loss(edge_weight,flops_criterion,meters,block_rng)
             loss = forward_loss(model, criterion, input, target, meters) + flops_loss # optimize w.r.t. CE Loss, flops loss
             loss.backward()
             optimizer.step()
@@ -311,8 +312,8 @@ def run_one_epoch(
             #current_macs, current_params = model_expected_profiling(model.module)
             #alphas = model.module.get_alphas()
             #loss_flops = torch.abs(current_macs / FLAGS.target_flops - 1.)
-            edge_weight = model_profiling(model.module)
-            flops_loss = get_flops_loss(edge_weight,flops_criterion,meters)
+            edge_weight,block_rng = model_profiling(model.module)
+            flops_loss = get_flops_loss(edge_weight,flops_criterion,meters,block_rng)
             loss = forward_loss(model, criterion, input, target, meters)
 
         batch_time = time.time() - end
@@ -401,7 +402,7 @@ def train_val_test():
     val_loader = data.val_loader
 
     criterion = torch.nn.CrossEntropyLoss(reduction="none").to(device)
-    flops_criterion = Flops_Loss(FLAGS.threshold,FLAGS.max_params).to(device)
+    flops_criterion = Flops_Loss(FLAGS.threshold,FLAGS.max_params,FLAGS.feature_dim).to(device)
     print("=> creating model '{}'".format(FLAGS.model))
     ##add threshold as param.
     model = getter("model")()